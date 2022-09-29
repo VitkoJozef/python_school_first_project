7909cd0846540f8f6151bb397b236330d3e5b812
Jozef Vitko <vitko@isw.sk>
2022-09-29 15:47:51 +0200

    Movie Library version 2

--- movie-library/services/MovieLibraryService.py
+++ movie-library/services/MovieLibraryService.py
@@ -1,16 +1,21 @@
+from typing import List, Any
+
 from models.MovieModel import MovieModel
+
+
 class MovieLibraryService:
 
     def __init__(self):
-        self.movies:list=[]
+        pass
 
-    def add_movie(self,movie:MovieModel):
+    movies: list[Any] = []
+
+    def add_movie(self, movie: MovieModel):
         self.movies.append(movie)
 
-    def remove_movie(self,index:int):
+    def remove_movie(self, index: int):
         self.movies.remove(index)
 
     def print_all(self):
         for movie in self.movies:
             print(movie.to_string2)
-
