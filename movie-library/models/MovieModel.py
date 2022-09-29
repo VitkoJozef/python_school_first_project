15dadac9eec7d08c3444a7d0a829c7e225e52b49
Jozef Vitko <vitko@isw.sk>
2022-09-29 14:34:48 +0200

    movieModel

---
+++ movie-library/models/MovieModel.py
@@ -0,0 +1,7 @@
+class Movie:
+    def __init__(self, name, description, category, authors):
+        self.category = category
+        self.description = description
+        self.authors = authors
+        self.name = name
+
